f9850efbcf1249ba65c265645ab1858efbf96479
Romadelf <romadelfatwork@hotmail.com>
2022-03-17 17:58:10 +0100

    Huge refactor and code clean + debug up to line 160

--- Trajectoire.py
+++ Trajectoire.py
@@ -2,54 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-haut_filet =1
 tol = 10**(-5) #tolerance 
-coef = 0.7   # coef de changement de vz
+coef = 0.7 # coef de changement de vz
 distance_maximal_terrain = 11.89
 
-def oderhs(t, y):
+def oderhs(t, instant_ball_data):
     
-    #definition des parametre du systeme
+    # definition des parametre du systeme
     d=0.0065
     m=0.058
     p=1.2
     C_d=0.065
     g=9.81
     
-    #ici on initialise notre repere orthonormé
+    # ici on initialise notre repere orthonormé
     Vec_u_x1=[1,0,0] #x
     Vec_u_x2=[0,1,0] #y
     Vec_u_x3=[0,0,1] #z
     
-    #x1= y[0]
-    #x2= y[1]      # ici on intialise la position car y est un vecteur   d'indice 0,1,2
-    #x3= y[2] 
-    
-    
-    #les forces  fdx et fm  a savoir les projection :
-    
-    #ici on prend les trois composante de la vitesse (angulaire et de translation)
-    v_x1 = y[3]
-    v_x2 = y[4]
-    v_x3 = y[5]
-    
+    # x1= instant_ball_data[0], x2= instant_ball_data[1] et x3= instant_ball_data[2] sont inutiles pour le calcul de la variation
 
-    w_x1 = y[6]
-    w_x2 = y[7]
-    w_x3 = y[8]
+    # les forces  fdx et fm  a savoir les projection :
+    
+    # ici on prend les trois composante de la vitesse (angulaire et de translation)
+    v_x1 = instant_ball_data[3]
+    v_x2 = instant_ball_data[4]
+    v_x3 = instant_ball_data[5]
+    
+    w_x1 = instant_ball_data[6]
+    w_x2 = instant_ball_data[7]
+    w_x3 = instant_ball_data[8]
    
-   
-   #puis on calcule leurs normes
-   
+    # puis on calcule leurs normes
    
     vitesse_translation=[v_x1, v_x2 ,v_x3]
     vitesse_angulaire  =[w_x1, w_x2 ,w_x3]
     
-    
     norme_de_v= np.linalg.norm(vitesse_translation)
     norme_de_w= np.linalg.norm(vitesse_angulaire)
    
-    #NORME DES FORCES 
+    # NORME DES FORCES 
     
     Force_frottement= C_d*p*(np.pi)*((d*d)/8)*((norme_de_v)**2)
     
@@ -57,14 +49,13 @@ def oderhs(t, y):
     
     Force_magnus=C_m*p*(np.pi)*((d*d)/8)*((norme_de_v)**2)
     
-    #ici on a un vecteur unitaire de la vitesse de translation
+    # ici on a un vecteur unitaire de la vitesse de translation
     
     direction_vitesse_translation= (vitesse_translation)/(norme_de_v)
     
-    
     # mainenant on determine les composantes grace au produit scalaire des deux 
-    #vecteur unitaire  pour obtenir nos trois composante
-    #ON FAIT (-)  la direction car c'est l'inverse de la vitesse
+    # vecteur unitaire  pour obtenir nos trois composante
+    # on fait (-) la direction car c'est l'inverse de la vitesse
     
     aux= np.dot(- direction_vitesse_translation,Vec_u_x1)
     fdx1= (Force_frottement)*(aux)
@@ -73,12 +64,9 @@ def oderhs(t, y):
     aux= np.dot(- direction_vitesse_translation,Vec_u_x3)
     fdx3= (Force_frottement)*(aux)
     
+    # fmx1? fmx2? fmx3? en fonction de leur direction
     
-    
-    #fmx1? fmx2? fmx3? en fonction de leur direction
-    
-    #ici on fait le produit vectorielle w vectoriel v
-    #puis on le norme 
+    # ici on fait le produit vectorielle w vectoriel v puis on le norme 
     
     Produit_vectoriel = np.cross(vitesse_translation,vitesse_angulaire)
     
@@ -93,133 +81,127 @@ def oderhs(t, y):
     aux1=np.dot(direction_force_magnus,Vec_u_x3)
     fmx3= (Force_magnus)*aux1
     
-    #Poid 
+    # Poids
     
     fgx1 = 0
     fgx2 = 0
     fgx3 =-m*g
-    
 
-    #definition du systeme
+    # definition du systeme
     
-    # on pose  X= dx1_dt              dX_dt = acceleration selon x1
-             # Y= dx2_dt   ainsi on a dY_dt = acceleration selon x2
-             # Z= dx3_dt              dZ_dt = acceleration selon x3
-   # vecteur y (x1,x2,x3,dx1_dt,dx2_dt,dx3_dt,w1,w2,w3)
-   # indice     0  1  2  3      4       5     6  7  8
-
-   
-   
+    # on pose :
+    # X= dx1_dt   ainsi on a  dX_dt = acceleration selon x1
+    # Y= dx2_dt               dY_dt = acceleration selon x2
+    # Z= dx3_dt               dZ_dt = acceleration selon x3
+    
+    # vecteur instant_ball_data (x1,x2,x3,dx1_dt,dx2_dt,dx3_dt,w1,w2,w3)
+    # indice     0  1  2  3      4       5     6  7  8
   
     dx1_dt = v_x1  # ici on  reprend les variables deja pris au debut pour les normes
     dx2_dt = v_x2  # on prend les dx_dt pour juste respecté les conventions
     dx3_dt = v_x3
     
-    
-    
     dy = np.zeros(9)
     
-    
+    # on met les trois composantes de la vitesse, 
     dy[0]= dx1_dt
-    dy[1]= dx2_dt      # on met les trois composantes de la vitesse, 
+    dy[1]= dx2_dt
     dy[2]= dx3_dt
     
+    # les trois composantes de l'acceleration
     dy[3]=(fgx1+fdx1+fmx1)*1/m
-    dy[4]=(fgx2+fdx2+fmx2)*1/m      # les trois composante de l'acceleration 
+    dy[4]=(fgx2+fdx2+fmx2)*1/m
     dy[5]=(fgx3+fdx3+fmx3)*1/m
     
-    #pas necessaire de remvoyer omega car tableau initialisé a zero
+    # pas necessaire de remvoyer omega car tableau initialisé a zero
     return dy
 
-
-
-def bouing(t, y):   #c'est notre event pour verifier quand x3vaut zero
-    return y[2]
+def bouing(t, instant_ball_data): # event pour verifier quand x3 vaut zero
+    return instant_ball_data[2]
 bouing.direction = -1
 bouing.terminal = True
 
-
-def trajectoireFiletHorizontal(yInit , T):
+def trajectoireFiletHorizontal(initial_ball_data, t_f):
     
-    #definition de parametre :
+    # définition des paramètres :
         
     frequence = 10**(6)
-    t0,i=0,0
-    nombre_iteration = np.linspace(t0,T,int(frequence*T))
+    instants_a_evaluer = np.linspace(0, t_f, int(frequence * t_f))
     
-    #verification des donnés :
-        
-        #autrement dit si ya pas de vitesse(translation et rotation) initial  ya pas de mouvements
-        #on renvoit la position initial
-          
-    if (abs(yInit[3])<=tol) and (abs(yInit[4])<=tol) and (abs(yInit[5])<=tol):
-      
-        return  [0,0,0] #position initial  au cas ou la vitesse initial est nul
+    # verification des donnés :
+    # si vitesse initial de translation nulle, il n'y a pas de mouvements
+    # comme cela pose des problèmes de division par 0 dans oderhs,
+    # on exclut ce cas, lequel est de toute façon anecdotique
+    # => à voir si on garde
+    if(abs(initial_ball_data[3]) <= tol and
+       abs(initial_ball_data[4]) <= tol and
+       abs(initial_ball_data[5]) <= tol):
+        return [0,0,0]
+    
+    pre_bounce_solve = solve_ivp(
+        oderhs,
+        [0, t_f],
+        initial_ball_data,
+        t_eval = instants_a_evaluer,
+        events = bouing,
+        rtol = 10**(-10),
+        atol = 10**(-25))
 
-        
-    variables_0 = solve_ivp(oderhs,[t0,T], yInit, t_eval = nombre_iteration, events = bouing,rtol =10**(-10) , atol = 10**(-25) )
-    arr_0 = variables_0.y
+    ball_data_timetable = pre_bounce_solve.y
     
-    position = arr_0 #on initialise position  
- 
-    
-    if variables_0.status == 1 : # = si il rebondi
-        # modification de new cond :
+    if pre_bounce_solve.status == 1 : # = si il rebondi
+        # manière d'aller chercher les dernières variables dans ball_data_timetable
+        # TODO_LOW possiblement plus clair d'utilisation via pre_bounce_solve.t.shape[0]
+        after_bounce_ball_data = ball_data_timetable[0:ball_data_timetable.shape[0], ball_data_timetable.shape[1] - 1]
+        after_bounce_ball_data[5] *= -coef
         
-        new_cond = arr_0[0:arr_0.shape[0], arr_0.shape[1] - 1] # manière d'aller chercher les dernières variables dans arr
-        new_cond[5] = -coef * new_cond[5]
-        
-        #reinitialisation des donné :
+        # reinitialisation des données :
+        # frequence * (delta_t) == on cree un tableau de frequence fixe allant du nouveau t_i au t_f donné 
+        # ce qui assure une precision a chaque rebond quel que soit t_f donné
         
         indice = np.shape(arr_0)[1] - 1
+        t_i = pre_bounce_solve.t[indice]
+        delta_t = t_f - t_i
+        instants_a_evaluer = np.linspace(t_i, t_f, int(frequence * delta_t))
         
-        t0 =variables_0.t[indice] #on reinitialise t0 
+        # on relance solve_ivp avec les nouvelles cond 
         
-        #frequence*(T-t0) == on cree un tableau de frequence fixe allant du nouveau t0 au T donné 
-        # ce qui assure une precision a chaque rebond quel que soit T donné
-        
-        nombre_iteration = np.linspace(t0,T,int(frequence*(T-t0)))
-        
-        #on relance solve_ivp avec les nouvelles cond 
-        
-        variables_1 = solve_ivp(oderhs,[t0,T],new_cond ,t_eval = nombre_iteration, events = bouing , rtol =10**(-10),atol = 10**(-25) )        
-        arr_1 = variables_1.y
+        post_bounce_solve = solve_ivp(
+            oderhs,
+            [t_i, t_f],
+            after_bounce_ball_data,
+            t_eval = instants_a_evaluer,
+            events = bouing,
+            rtol = 10**(-10),
+            atol = 10**(-25))
            
-        position  = np.concatenate((arr_0, arr_1), axis=1) # on regroupe les tableaux
-        
+        ball_data_timetable = np.concatenate((ball_data_timetable, post_bounce_solve.y), axis=1) # on regroupe les tableaux
 
     # debut :
-        
-
     
-  
-    x1=position[0]  #longeur
-    x2=position[1]  #largeur
-    x3=position[2]  #hauteur
+    x=position[0]  #longeur
+    y=position[1]  #largeur
+    z=position[2]  #hauteur
     
-
+    # passe le filet ou pas ?
+    # =>  on reinitialise frequence comme l'indice maximal de y
     
-    #passe le filet   ou pas 
-    
-    
-   # on reinitialise frequence comme l'indice maximal de y 
-    aux = np.shape(x1) #car shape(x1) = shape(x2) = shape(x3)  
-    frequence = aux[0] 
-   
-    while i <frequence and x1[i]<=tol: #tant que  on est avant le filet 
-        if  x3[i] <= hauteur_filet(x2[i]) :  # et si la hauteur de la balle est inferieur a celle du filet 
+    frequence = np.shape(x)[0] # car shape(x) = shape(y) = shape(z)
+    i = 0
+    while i < frequence and x[i] <= tol: #tant que  on est avant le filet 
+        if  z[i] <= hauteur_filet(y[i]) :  # et si la hauteur de la balle est inferieur a celle du filet 
                 return [0,0,0] #erreur 
-        i +=1
+        i += 1
         
-    Dernier_indice = frequence-1  #parce que indice va de 0 a n-1
+    dernier_indice = frequence - 1  #parce que indice va de 0 a n-1
     return [
-        x1[Dernier_indice],
-        x2[Dernier_indice],
-        x3[Dernier_indice]
+        x[dernier_indice],
+        y[dernier_indice],
+        z[dernier_indice]
     ]
 
 def hauteur_filet(dist_centre):
-    return haut_filet
+    return 1 # temporaire, pour le milestone 2
 #    bord = 8.23 / 2 + 0.914 # = 5.029
 #    h_nominale = 1.07
 #    delta_h = h_nominale - 0.914
