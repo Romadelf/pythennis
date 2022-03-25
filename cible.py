4af74803454259bc41edff843c0eccf79ebb87a0
AKRAM <musehassanarham@gmail.com>
2022-03-25 14:57:41 +0100

    ciblerebond

--- cible.py
+++ cible.py
@@ -7,33 +7,61 @@
 #omega, statut = rechercheOmega(y0,cibleRebond)
 #omega, statut = rechercheOmega2(y0,cibleHauteur)
 import Trajectoire as Tj
-import RechercheRacine as sol
+import RechercheRacine as R
 import numpy as np
+#Condition initial : 
 
-def rechercheHauteur(y0,cibleRebond): 
-    t_max = 10 
-    haut_init = y0[2]
-    position =  Tj.trajectoireFiletHorizontal(y0,t_max )
-    pas = 0.1
-    haut_max =5
-    tol = 1/100
-    i =1
-    n_max=100
+y0 = [-11.89,0,2,50,1,0,30,15,0]    
+
+#parametre : 
+    
+t_i , t_f = 0, 4  #t_span 
+n_point = 200  #nombre de point 
+instants_a_evaluer = np.linspace(t_i, t_f,n_point ) 
   
+statut  , error = 0 , 42
+tol =  1/1000 #tolerance au millimetre 
+pas = 1/100 #1/100 DE METRE donc du centimetre 
+haut_min =2 # m
+haut_max =3 # m
+
+z0 , z1 = 0 , 10
+
+
+
+      
+def rechercheHauteur(y0,cibleRebond): 
+    #argument de fonction : 
+    def f_trajectoire(h):
+        #Definition d'event
+        def event_hauteur (t,y0):
+            return y0[2]
+        event_hauteur.direction = -1
+        event_hauteur.terminal = True
+
     
-    while  abs(position[0]-cibleRebond)>tol :
+        y0[2] =  h 
+        solve = Tj.solve_ivp(
+            Tj.oderhs,
+            [t_i, t_f],
+            y0,
+            t_eval = instants_a_evaluer,
+            events =event_hauteur ,
+            rtol = 10**(-5),
+            atol = 10**(-8))
     
-        if position[0]<=cibleRebond:
-            
-            y0[2] += pas
-            position =  Tj.trajectoireFiletHorizontal(y0,t_max )
-        else :
-            y0[2] -= pas
-            position =  Tj.trajectoireFiletHorizontal(y0,t_max ) 
-            
-    hauteur =position[2]
-            
+        timetable = solve.y
+        x1_rebond = timetable [0,-1]  #x1 est l'abcisse 
+        return x1_rebond - cibleRebond #on retourn la difference pour savoir quand c'est zero 
+    return R.bissection(f_trajectoire, z0, z1, tol) #bissection car se comporte mieux 
+        
+        
+
     
-    return hauteur
+  
+        
+      
     
-#event = lambda t, y: y[2]
+
+                               
+
