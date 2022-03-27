81fcad8da7aea68783b1783b27812372f5e85a8f
Romadelf <romadelfatwork@hotmail.com>
2022-03-27 19:29:10 +0200

    recherche_hauteur_2 init

---
+++ recherche_hauteur_2.py
@@ -0,0 +1,21 @@
+
+def rechercheHauteur2(y0, cibleHauteur):
+    """Renvoie la hauteur initiale nécessaire d'une balle dont les autres composantes sont fixées pour qu'elle atteigne une hauteur cible donnée au niveau de la ligne de fond, après rebond.
+#TODO
+    Paramètres
+    ----------
+    y0 : vecteur des composantes # preciser les y0[0 à 8]
+    op : callable
+        This should be either numpy.amin or `numpy.amax` or `numpy.sum`.
+
+    Returns
+    -------
+    result : float or ndarray
+        If `x` is 2-D, the return values is a float.
+        Otherwise, it is an array with ``x.ndim - 2`` dimensions.
+        The return values are either the minimum or maximum or sum of the
+        singular values of the matrices, depending on whether `op`
+        is `numpy.amin` or `numpy.amax` or `numpy.sum`.
+    """
+    print('ok')
+    return 0
